2503adc98ed468d3142a6d896942cb9c6b51a006
KenLin <qq1607019322@gmail.com>
2022-06-29 03:45:06 +1000

    a

--- flask_app.py
+++ flask_app.py
@@ -7,45 +7,52 @@ app = Flask(__name__)
 hops = hs.Hops(app)
 
 @hops.component(
+    #http://127.0.0.1:5000/calculate_the_angle
+    "/calculate_the_angle",
     name = 'calculate_the_angle',
-    description = "cal_the_angle",
+    description = "calculate_the_angle",
     inputs = [
-        hs.HopsNumber('p1','p1','p1'),
-        hs.HopsNumber('p2','p2','p2'),
-        hs.HopsNumber('p3','p3','p3')
+        #access = hs.HopsParamAccess.LIST
+        hs.HopsNumber("p_1"),
+        hs.HopsNumber("p_2"),
+        hs.HopsNumber("p_3")
     ],
     outputs = [
-        hs.HopsNumber("the_angle")
+        hs.HopsNumber("angle")
     ]
 )
 
-@app.route('/')
-def hello_world():
-    return 'Hello hello hello hello Hello!'
+#@app.route('/')
+#def hello_world():
+ #   return 'Hello hello hello hello Hello!'
 
-
-@app.route('/abcd')
-def abcd():
-    return 'have no idea yet'
-
-@app.route('calculate_the_angle')
-def calculate_triangle_angle(p1,p2,p3):
+@app.route('/cal_the_angle')
+def calculate_the_angle(p_1,p_2,p_3):
     import math
-    args = request.args
-    variables = dict(args)
+   # args = request.args
+   # variables = dict(args)
+    #float(p1)
+    #float(p2)
+    #float(p3)
+    p1 = []
+    for num in p_1:
+        p3.append(float(num))
 
+    p2 = []
+    for num in p_2:
+        p3.append(float(num))
 
-#    let { x: x1, y: y1, z: z1 } = p1;
- #   let { x: x2, y: y2, z: z2 } = p2;
-  #  let { x: x3, y: y3, z: z3 } = p3;
-    
-    v_p1_p2 = math.sqrt((p1[1] - p1[0])**2 + (p2[1]-p2[0])**2 +p3[1]-p3[0] **2)
-    v_p2_p3 = math.sqrt((p1[2] - p1[1])**2 + (p2[2]-p2[1])**2 +p3[2]-p3[1] **2)
+    p3 = []
+    for num in p_3:
+        p3.append(float(num))
+    v_p1_p2 = math.sqrt((p1[1] - p1[0])**2 + (p2[1]-p2[0])**2 +(p3[1]-p3[0]) **2)
+    v_p2_p3 = math.sqrt((p1[2] - p1[1])**2 + (p2[2]-p2[1])**2 +(p3[2]-p3[1])**2)
 
     aaa = (p1[0] - p1[1])*(p1[2] - p1[1])+(p2[0]-p2[1])*(p2[2] - p2[1])+(p3[0] - p3[1])*(p3[2]-p3[1])
 
-    the_angle = (math.acos(aaa/(v_p1_p2 *v_p2_p3))/math.pi) * 180
-    return the_angle
+    angle = (math.acos(aaa/(v_p1_p2 *v_p2_p3))/math.pi) * 180
+    #angle = p1+p2+p3
+    return angle
 
 if __name__ == '__main__':
     app.run()
