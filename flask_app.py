fd61d84dce8bd285b8eb5a7b7abec9ce43471c5c
KenLin <qq1607019322@gmail.com>
2022-06-30 14:22:52 +1000

    w

--- flask_app.py
+++ flask_app.py
@@ -41,4 +41,54 @@ if __name__ == '__main__':
 @app.route('/hello')
 def hello():
     return 'Hello hello hello, Welcome'
+
+#@hops.component(
+    #http://127.0.0.1:5000/calculate_the_angle
+    #using coordinate to angle angle
+ #   "/calculate_the_angle",
+ #   name = 'calculate_the_angle',
+ #   description = "calculate_the_angle",
+ #   inputs = [
+        #access = hs.HopsParamAccess.LIST
+ #       hs.HopsNumber("p_1",hs.HopsParamAccess.LIST),
+ #       hs.HopsNumber("p_2",hs.HopsParamAccess.LIST),
+ #       hs.HopsNumber("p_3",hs.HopsParamAccess.LIST)
+ #   ],
+ #   outputs = [
+ #       hs.HopsNumber("angle")
+ #   ]
+#)
+
+#@app.route('/')
+#def hello_world():
+ #   return 'Hello hello hello hello Hello!'
+
+#@app.route('/cal_the_angle')
+#def calculate_the_angle(p_1,p_2,p_3):
+    #import math
+   # args = request.args
+   # variables = dict(args)
+    #float(p1)
+    #float(p2)
+    #float(p3)
+    #p1 = []
+    #for num in p_1:
+        #p1.append(float(num))
+
+    #p2 = []
+    #for num in p_2:
+        #p2.append(float(num))
+
+    #p3 = []
+    #for num in p_3:
+        #p3.append(float(num))
+
+    #v_p1_p2 = math.sqrt((p1[1] - p1[0])**2 + (p2[1]-p2[0])**2 +(p3[1]-p3[0]) **2)
+    #v_p2_p3 = math.sqrt((p1[2] - p1[1])**2 + (p2[2]-p2[1])**2 +(p3[2]-p3[1])**2)
+
+    #aaa = (p1[0] - p1[1])*(p1[2] - p1[1])+(p2[0]-p2[1])*(p2[2] - p2[1])+(p3[0] - p3[1])*(p3[2]-p3[1])
+
+    #angle = (math.acos(aaa/(v_p1_p2 *v_p2_p3))/math.pi) * 180
+    #angle = p1+p2+p3
+    #return angle
     
